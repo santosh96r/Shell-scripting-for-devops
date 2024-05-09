905b997a1bd8ce68e904e606a75ec97fda4f09c3
santosh96r <santoshragar87@gmail.com>
2024-05-09 22:40:57 +0530

    testing

--- hello.sh
+++ hello.sh
@@ -1,5 +1,20 @@
-this is my new file
+#!/bin/bash
 
-<new changes i want to do >need to check the conflict issue 
+<<comment 
+anything 
+written 
+here , wont be executed 
+comment
 
-i want to push my file to remote repo 
+echo " tws : hello dosto "
+
+echo "learners : we will subscribe the channel ok!"
+
+name="jetha"
+echo " name is $name and date is $(date)"
+
+echo "enter your name : "
+
+read username 
+
+echo " you entered $username"
